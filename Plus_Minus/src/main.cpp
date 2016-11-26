3debf8a265beb7cabd861b28e507bf3754cb6032
Daniel <dmoody256@gmail.com>
2016-11-26 17:17:19 -0500

    added Plus_Minus challenge

---
+++ Plus_Minus/src/main.cpp
@@ -0,0 +1,91 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <exception>
+#include <stdlib.h>
+#include <stdexcept>
+
+// setup Catch unit testing if this is a test build
+// it will create a new main for us
+#ifdef UNIT_TEST_BUILD
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#endif
+
+// Debug print is useful for quickly debugging
+// output for things that may happen over time
+#ifdef DEBUG_PRINT
+#define D(x) x
+#else
+#define D(x)
+#endif
+
+// This challenge can be found here
+//https://www.hackerrank.com/challenges/plus-minus
+
+
+//     _____    ____    _____    ______
+//    / ____|  / __ \  |  __ \  |  ____|
+//   | |      | |  | | | |  | | | |__
+//   | |      | |  | | | |  | | |  __|
+//   | |____  | |__| | | |__| | | |____
+//    \_____|  \____/  |_____/  |______|
+//
+// letters generated at http://patorjk.com/software/taag
+
+
+
+#ifdef TEST_BUILD
+//    _______   ______    _____   _______
+//   |__   __| |  ____|  / ____| |__   __|
+//      | |    | |__    | (___      | |
+//      | |    |  __|    \___ \     | |
+//      | |    | |____   ____) |    | |
+//      |_|    |______| |_____/     |_|
+//
+// letters generated at http://patorjk.com/software/taag
+
+// control the number of random generated tests
+#define NUM_TESTS 1000
+
+
+
+#endif
+
+
+
+#ifndef CATCH_CONFIG_MAIN
+//    __  __              _____   _   _
+//   |  \/  |     /\     |_   _| | \ | |
+//   | \  / |    /  \      | |   |  \| |
+//   | |\/| |   / /\ \     | |   | . ` |
+//   | |  | |  / ____ \   _| |_  | |\  |
+//   |_|  |_| /_/    \_\ |_____| |_| \_|
+//
+// letters generated at http://patorjk.com/software/taag
+
+int main (int argc, char *argv[]) {
+
+	std::string inputValue;
+	while (std::cin >> inputValue) {
+
+		// we expect the first value to be the number of
+		// operons to read in on the next line
+		int operons = atoi(inputValue.c_str());
+
+		for (int i = 0; i < operons; i++) {
+			// read in each next number and
+			// add it to the result
+			std::cin >> inputValue;
+		}
+
+		// print the final result
+		std::cout << inputValue << std::endl;
+	}
+
+	return 0;
+}
+#endif
