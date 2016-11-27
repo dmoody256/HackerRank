aa0bc9e8c2c3d3bf5755386b04698cc55b3aa281
Daniel <dmoody256@gmail.com>
2016-11-27 17:56:37 -0500

    added simple class and tests, seems to be done and working

--- Staircase/src/main.cpp
+++ Staircase/src/main.cpp
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <stdexcept>
 
-//#define UNIT_TEST_BUILD
+#define UNIT_TEST_BUILD
 
 // setup Catch unit testing if this is a test build
 // it will create a new main for us
@@ -38,7 +38,39 @@
 //
 // letters generated at http://patorjk.com/software/taag
 
+class Staircase {
 
+public:
+
+	Staircase() {
+		_size = 0;
+	}
+
+	Staircase(int const &size) {
+		_size = size;
+
+		std::stringstream sstr;
+		for (int line = 1; line <= size; line++) {
+			for (int spaces = size - line; spaces > 0; spaces--)
+				sstr << " ";
+
+			for (int symbols = 0; symbols < line; symbols++)
+				sstr << "#";
+
+			sstr << std::endl;
+		}
+		_staircase = sstr.str();
+	}
+
+	std::string ToString() {
+		return _staircase;
+	}
+
+private:
+
+	int _size;
+	std::string _staircase;
+};
 
 #ifdef UNIT_TEST_BUILD
 //    _______   ______    _____   _______
@@ -52,6 +84,108 @@
 
 // control the number of random generated tests
 #define NUM_TESTS 1000
+
+TEST_CASE("This test will create and add big ints from generated and hardcoded values", "[BigInt]") {
+
+	// setup the varibale we use for the addition tests
+	int testCountArray[NUM_TESTS];
+	int realCountArray[NUM_TESTS];
+
+	std::cout << "Generating " << NUM_TESTS << " test values for operator+ test:" << std::endl;
+	for (int i = 0; i < NUM_TESTS; i++) {
+		std::cout << "Generating... " << int((float)i / (float)NUM_TESTS * 100.0) << "%\r";
+		std::cout.flush();
+
+		// create a random bigint and will see if we the same exact string
+		// back out
+		Staircase testStaircase = Staircase(i);
+		std::string testString = testStaircase.ToString();
+		testCountArray[i] = std::count(testString.begin(), testString.end(), '#');
+		realCountArray[i] = (i * (i + 1)) / 2;
+	}
+
+	std::string StaircaseTest2;
+	StaircaseTest2.append("    #\n");
+	StaircaseTest2.append("   ##\n");
+	StaircaseTest2.append("  ###\n");
+	StaircaseTest2.append(" ####\n");
+	StaircaseTest2.append("#####\n");
+
+	std::string StaircaseTest1;
+	StaircaseTest1.append("         #\n");
+	StaircaseTest1.append("        ##\n");
+	StaircaseTest1.append("       ###\n");
+	StaircaseTest1.append("      ####\n");
+	StaircaseTest1.append("     #####\n");
+	StaircaseTest1.append("    ######\n");
+	StaircaseTest1.append("   #######\n");
+	StaircaseTest1.append("  ########\n");
+	StaircaseTest1.append(" #########\n");
+	StaircaseTest1.append("##########\n");
+
+	std::string StaircaseTest3;
+	StaircaseTest3.append("                                       #\n");
+	StaircaseTest3.append("                                      ##\n");
+	StaircaseTest3.append("                                     ###\n");
+	StaircaseTest3.append("                                    ####\n");
+	StaircaseTest3.append("                                   #####\n");
+	StaircaseTest3.append("                                  ######\n");
+	StaircaseTest3.append("                                 #######\n");
+	StaircaseTest3.append("                                ########\n");
+	StaircaseTest3.append("                               #########\n");
+	StaircaseTest3.append("                              ##########\n");
+	StaircaseTest3.append("                             ###########\n");
+	StaircaseTest3.append("                            ############\n");
+	StaircaseTest3.append("                           #############\n");
+	StaircaseTest3.append("                          ##############\n");
+	StaircaseTest3.append("                         ###############\n");
+	StaircaseTest3.append("                        ################\n");
+	StaircaseTest3.append("                       #################\n");
+	StaircaseTest3.append("                      ##################\n");
+	StaircaseTest3.append("                     ###################\n");
+	StaircaseTest3.append("                    ####################\n");
+	StaircaseTest3.append("                   #####################\n");
+	StaircaseTest3.append("                  ######################\n");
+	StaircaseTest3.append("                 #######################\n");
+	StaircaseTest3.append("                ########################\n");
+	StaircaseTest3.append("               #########################\n");
+	StaircaseTest3.append("              ##########################\n");
+	StaircaseTest3.append("             ###########################\n");
+	StaircaseTest3.append("            ############################\n");
+	StaircaseTest3.append("           #############################\n");
+	StaircaseTest3.append("          ##############################\n");
+	StaircaseTest3.append("         ###############################\n");
+	StaircaseTest3.append("        ################################\n");
+	StaircaseTest3.append("       #################################\n");
+	StaircaseTest3.append("      ##################################\n");
+	StaircaseTest3.append("     ###################################\n");
+	StaircaseTest3.append("    ####################################\n");
+	StaircaseTest3.append("   #####################################\n");
+	StaircaseTest3.append("  ######################################\n");
+	StaircaseTest3.append(" #######################################\n");
+	StaircaseTest3.append("########################################\n");
+
+	// perform the to string tests to make sure our BigInt class is not
+	// destroying the data
+	SECTION("Performing tests") {
+
+		std::string result = Staircase(10).ToString();
+		REQUIRE(StaircaseTest1.compare(result) == 0);
+		result = Staircase(5).ToString();
+		REQUIRE(StaircaseTest2.compare(result) == 0);
+		result = Staircase(40).ToString();
+		REQUIRE(StaircaseTest3.compare(result) == 0);
+
+		for (int i = 0; i < NUM_TESTS; i++) {
+			REQUIRE(realCountArray[i] == testCountArray[i]);
+		}
+
+
+	}
+
+}
+
+#endif
 
 #ifndef CATCH_CONFIG_MAIN
 //    __  __              _____   _   _
