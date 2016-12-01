4229be5daaa2bbe3782687f9c81edb2e3181c13e
Daniel <dmoody256@gmail.com>
2016-11-30 23:53:00 -0500

    added basic class structure, the class will sort as values are added by organzing them in sorted fashion. Still need to make tests and also replace strings with dashes in the first half of items in the array.

--- The_Full_Counting_Sort/src/main.cpp
+++ The_Full_Counting_Sort/src/main.cpp
@@ -39,6 +39,34 @@
 //
 // letters generated at http://patorjk.com/software/taag
 
+class SortedArray {
+
+public:
+
+	SortedArray() {
+		_size = 0;
+	}
+
+	void PushValue(int const &position, std::string const &string) {
+		
+		ListItem item;
+		item.originalPosition = _size;
+		item.string = string;
+		_array[position].push_back(item);
+		_size++;
+	}
+
+private:
+
+	struct ListItem {
+		int originalPosition;
+		std::string string;
+	} typedef ListItem;
+	int _size;
+	std::map< int, std::vector< ListItem > > _array;
+
+};
+
 
 #ifdef UNIT_TEST_BUILD
 //    _______   ______    _____   _______
@@ -52,6 +80,27 @@
 
 // control the number of random generated tests
 #define NUM_TESTS 1000
+
+TEST_CASE("Testing adding values to the sorted array") {
+
+	SortedArray testArray;
+	
+	int randTestSize = rand() % 100;
+	for (int i = 0; i < randTestSize; i++) {
+		int position = rand() % 26;
+		int stringsize = rand() % 10;
+		std::string nextString;
+		for (int j = 0; j < stringsize; j++) {
+			nextString.append(1, position + 65);
+		}
+		testArray.PushValue(position, nextString);
+	}
+
+	int test = 5;
+
+	
+
+}
 
 
 #endif
