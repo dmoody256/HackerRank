ee5efc7c7249f5f944e6e718aa917887b1f1cbf5
Daniel <dmoody256@gmail.com>
2017-02-02 22:15:27 -0500

    just moved the print again from Insertion Sort Part 2

---
+++ Sorting/Correctness_and_the_Loop_Variant/src/main.cpp
@@ -0,0 +1,123 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <deque>
+#include <sstream>
+#include <exception>
+#include <stdlib.h>
+#include <map>
+#include <stdexcept>
+#include <functional>
+#include <string.h> // memcpy
+
+//#define UNIT_TEST_BUILD
+
+// setup Catch unit testing if this is a test build
+// it will create a new main for us
+#ifdef UNIT_TEST_BUILD
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#endif
+
+// Debug print is useful for quickly debugging
+// output for things that may happen over time
+#ifdef DEBUG_PRINT
+#define D(x) x
+#else
+#define D(x)
+#endif
+
+// This challenge can be found here
+//https://www.hackerrank.com/challenges/find-the-median
+
+
+//     _____    ____    _____    ______
+//    / ____|  / __ \  |  __ \  |  ____|
+//   | |      | |  | | | |  | | | |__
+//   | |      | |  | | | |  | | |  __|
+//   | |____  | |__| | | |__| | | |____
+//    \_____|  \____/  |_____/  |______|
+//
+// letters generated at http://patorjk.com/software/taag
+class InsertionSort {
+
+public:
+
+	int PrintShifts(int const size, const int* const list) const {
+		int* array = new int[size];
+		int shifts = 0;
+		memcpy(array, list, size * sizeof(int));
+		for (int i = 1; i < size; i++) {
+			int temp = array[i];
+			int j = i - 1;
+			while (j >= 0 && array[j] > temp) {
+				array[j + 1] = array[j];
+
+				j = j - 1;
+			}
+			array[j + 1] = temp;
+			
+		}
+		for (int k = 0; k < size; k++) {
+			std::cout << array[k];
+			std::cout << ' ';
+		}
+		std::cout << std::endl;
+		delete[] array;
+		return shifts;
+	}
+};
+
+
+#ifdef UNIT_TEST_BUILD
+//    _______   ______    _____   _______
+//   |__   __| |  ____|  / ____| |__   __|
+//      | |    | |__    | (___      | |
+//      | |    |  __|    \___ \     | |
+//      | |    | |____   ____) |    | |
+//      |_|    |______| |_____/     |_|
+//
+// letters generated at http://patorjk.com/software/taag
+
+// control the number of random generated tests
+#define NUM_TESTS 200
+
+TEST_CASE("Testing adding values to the sorted array") {
+
+
+}
+
+#endif
+
+#ifndef CATCH_CONFIG_MAIN
+//    __  __              _____   _   _
+//   |  \/  |     /\     |_   _| | \ | |
+//   | \  / |    /  \      | |   |  \| |
+//   | |\/| |   / /\ \     | |   | . ` |
+//   | |  | |  / ____ \   _| |_  | |\  |
+//   |_|  |_| /_/    \_\ |_____| |_| \_|
+//
+// letters generated at http://patorjk.com/software/taag
+
+int main(int argc, char *argv[]) {
+
+	std::string inputValue;
+	while (std::cin >> inputValue) {
+
+		int size = std::atoi(inputValue.c_str());
+		std::vector<int> array;
+		for (int i = 0; i < size; i++) {
+			int value;
+			std::cin >> value;
+			array.push_back(value);
+		}
+		InsertionSort insertionArray;
+		insertionArray.PrintShifts(size, &array.at(0));
+
+	}
+
+	return 0;
+}
+#endif
